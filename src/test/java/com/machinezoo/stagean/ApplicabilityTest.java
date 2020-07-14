806756121324d1d5548a92533ad822afb394a672
Robert Važan <robert.vazan@protonmail.com>
2020-07-14 17:44:10 +0000

    Comprehensive tests

--- src/test/java/com/machinezoo/stagean/ApplicabilityTest.java
+++ src/test/java/com/machinezoo/stagean/ApplicabilityTest.java
@@ -1,15 +1,105 @@
 // Part of Stagean: https://stagean.machinezoo.com/
 package com.machinezoo.stagean;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
-@CompleteTests
+/*
+ * Type target.
+ */
+@CompleteCode
+@DraftApi
+@StubTests
+@NoDocs
 public class ApplicabilityTest {
+	/*
+	 * All annotation applied.
+	 */
+	@NoTests
+	@NoDocs
+	public static void none() {
+	}
+	@StubCode
+	@StubApi
+	@StubTests
+	@StubDocs
+	public static void stub() {
+	}
+	@DraftCode
+	@DraftApi
+	@DraftTests
+	@DraftDocs
+	public static void draft() {
+	}
+	@CompleteCode
+	@CompleteApi
+	@CompleteTests
+	@CompleteDocs
+	public static void complete() {
+	}
+	/*
+	 * Field target.
+	 */
+	@CompleteCode
+	@DraftApi
+	@StubTests
+	@NoDocs
+	public static int field;
+	/*
+	 * Constructor target.
+	 */
+	@CompleteCode
+	@DraftApi
+	@StubTests
+	@NoDocs
+	public ApplicabilityTest() {
+	}
+	public static enum EnumTarget {
+		ONE,
+		/*
+		 * Enum constant target.
+		 */
+		@CompleteCode
+		@DraftApi
+		@StubTests
+		@NoDocs
+		TWO;
+	}
+	/*
+	 * Annotation target.
+	 */
+	@CompleteCode
+	@DraftApi
+	@StubTests
+	@NoDocs
+	public static @interface AnnotationTarget {
+	}
+	/*
+	 * Method target.
+	 */
+	@CompleteCode
+	@DraftApi
+	@StubTests
+	@NoDocs
+	public static void method() {
+	}
+	/*
+	 * Text parameter.
+	 */
+	@CompleteCode("test")
+	@DraftApi("test")
+	@StubTests("test")
+	@NoDocs("test")
+	public static void parameter() {
+	}
 	@Test
-	@NoDocs("just a test")
 	public void load() {
 		/*
 		 * Intentionally left empty. We just want to check that the annotations compile and the test class is loaded.
 		 */
+	}
+	@Test
+	public void runtime() {
+		assertEquals(0, ApplicabilityTest.class.getAnnotations().length);
 	}
 }
---
+++ src/test/java/com/machinezoo/stagean/pkg/package-info.java
@@ -0,0 +1,9 @@
+// Part of Stagean: https://stagean.machinezoo.com/
+/*
+ * Package target.
+ */
+@com.machinezoo.stagean.CompleteCode
+@com.machinezoo.stagean.DraftApi
+@com.machinezoo.stagean.StubTests
+@com.machinezoo.stagean.NoDocs
+package com.machinezoo.stagean.pkg;
