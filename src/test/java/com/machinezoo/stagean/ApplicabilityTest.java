d6e423904f2c7380eacb315148bf542e25a5cefa
Robert Važan <robert.vazan@protonmail.com>
2020-07-14 16:40:54 +0000

    Unit test

---
+++ src/test/java/com/machinezoo/stagean/ApplicabilityTest.java
@@ -0,0 +1,15 @@
+// Part of Stagean: https://stagean.machinezoo.com/
+package com.machinezoo.stagean;
+
+import org.junit.jupiter.api.*;
+
+@NoDocs
+public class ApplicabilityTest {
+	@Test
+	@NoDocs("just a test")
+	public void load() {
+		/*
+		 * Intentionally left empty. We just want to check that the annotations compile and the test class is loaded.
+		 */
+	}
+}
