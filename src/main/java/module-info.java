c25828d4643149cbe92072a5409098b971004577
Robert Važan <robert.vazan@tutanota.com>
2021-12-23 18:25:29 +0000

    Module javadoc

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,8 @@
 // Part of Stagean: https://stagean.machinezoo.com/
+/**
+ * Annotation types that let you document development stage of your project on class or method level.
+ * See {@link com.machinezoo.stagean} package.
+ */
 module com.machinezoo.stagean {
 	exports com.machinezoo.stagean;
 }
