ca2b502cc0e51ff1df2f2b1a5762df42651c7b38
Robert Važan <robert.vazan@tutanota.com>
2022-08-03 16:13:42 +0000

    Updated project config: UA banner, upgraded javadoc plugin, build pull requests, note about generated code

--- scripts/configure.py
+++ scripts/configure.py
@@ -2,11 +2,10 @@
 
 # We are assuming that project-config is available in sibling directory.
 # Checkout from https://github.com/robertvazan/project-config
-import os.path
-import sys
-sys.path.append(os.path.normpath(os.path.join(__file__, '../../../project-config/src')))
-
-from java import *
+import pathlib
+project_directory = lambda: pathlib.Path(__file__).parent.parent
+config_directory = lambda: project_directory().parent/'project-config'
+exec((config_directory()/'src'/'java.py').read_text())
 
 project_script_path = __file__
 repository_name = lambda: 'stagean'
@@ -19,4 +18,4 @@ test_coverage = lambda: False
 def dependencies():
     use_junit()
 
-generate(globals())
+generate()
